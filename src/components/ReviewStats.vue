b3f85ec5c790f44df590f382ee0b26e3b0138580
mirza-sync <mirzasyahmi123@gmail.com>
2023-02-20 16:45:10 +0800

    Add: review stats ui

---
+++ src/components/ReviewStats.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="feedback-stats">
+    <h4>3 reviews</h4>
+    <h4>Average rating 9.4</h4>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup>
 import ReviewForm from "@/components/ReviewForm.vue";
+import ReviewStats from "@/components/ReviewStats.vue";
 </script>
 
 <template>
   <div class="home">
     <div class="container">
       <ReviewForm />
+      <ReviewStats />
     </div>
   </div>
 </template>
