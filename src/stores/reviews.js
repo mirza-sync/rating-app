407a991e00a201cdcfd91e0cd81825d66350faf4
mirza-sync <mirzasyahmi123@gmail.com>
2023-02-21 16:39:51 +0800

    Add reviews store

---
+++ src/stores/reviews.js
@@ -0,0 +1,24 @@
+import { ref, computed, reactive } from "vue";
+import { defineStore } from "pinia";
+
+export const useReviewsStore = defineStore("reviews", () => {
+  const reviews = ref([]);
+  const editedData = reactive({
+    editable: false,
+    item: null,
+  });
+
+  async function addReview(review) {
+    const response = await fetch("http://localhost:5000/reviews", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(review),
+    });
+    const newReview = await response.json();
+    reviews.value = [newReview, ...reviews.value];
+  }
+
+  return { reviews, addReview };
+});
